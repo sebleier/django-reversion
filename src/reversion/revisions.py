ad7111ca8cabb6a62c399adcba84091e467c2a02
Sean Bleier <sebleier@gmail.com>
2011-06-29 15:33:27 -0700

    Added more robust handling for related objects

--- src/reversion/revisions.py
+++ src/reversion/revisions.py
@@ -11,6 +11,7 @@ from threading import local
 from django.contrib.contenttypes.models import ContentType
 from django.core import serializers
 from django.db import models
+from django.db.models.fields import FieldDoesNotExist
 from django.db.models.query import QuerySet
 from django.db.models.signals import post_save, post_delete
 
@@ -19,39 +20,39 @@ from reversion.storage import VersionFileStorageWrapper
 
 
 class RevisionManagementError(Exception):
-    
+
     """
     Exception that is thrown when something goes wrong with revision managment.
     """
-    
+
 
 class RegistrationError(Exception):
-    
+
     """Exception thrown when registration with Reversion goes wrong."""
 
 
 class RegistrationInfo(object):
-    
+
     """Stored registration information about a model."""
-    
+
     __slots__ = "fields", "file_fields", "follow", "format",
-    
+
     def __init__(self, fields, file_fields, follow, format):
         """Initializes the registration info."""
         self.fields = fields
         self.file_fields = file_fields
         self.follow = follow
         self.format = format
-          
-          
+
+
 class RevisionState(local):
-    
+
     """Manages the state of the current revision."""
-    
+
     def __init__(self):
         """Initializes the revision state."""
         self.clear()
-    
+
     def clear(self):
         """Puts the revision manager back into its default state."""
         self.objects = set()
@@ -60,17 +61,17 @@ class RevisionState(local):
         self.depth = 0
         self.is_invalid = False
         self.meta = []
-   
+
 
 DEFAULT_SERIALIZATION_FORMAT = "json"
-   
-   
+
+
 class RevisionManager(object):
-    
+
     """Manages the configuration and creation of revisions."""
-    
+
     __slots__ = "__weakref__", "_registry", "_state",
-    
+
     def __init__(self):
         """Initializes the revision manager."""
         self._registry = {}
@@ -84,7 +85,7 @@ class RevisionManager(object):
         manager.
         """
         return model_class in self._registry
-        
+
     def register(self, model_class, fields=None, follow=(), format=DEFAULT_SERIALIZATION_FORMAT):
         """Registers a model with this revision manager."""
         # Prevent multiple registration.
@@ -114,7 +115,7 @@ class RevisionManager(object):
         post_save.connect(self.post_save_receiver, model_class)
         post_delete.connect(self.post_delete_receiver, model_class)
 
-    
+
     def get_registration_info(self, model_class):
         """Returns the registration information for the given model class."""
         try:
@@ -123,7 +124,7 @@ class RevisionManager(object):
             raise RegistrationError, "%r has not been registered with Reversion." % model_class
         else:
             return registration_info
-        
+
     def unregister(self, model_class):
         """Removes a model from version control."""
         try:
@@ -134,76 +135,76 @@ class RevisionManager(object):
             for field in registration_info.file_fields:
                 field.storage = field.storage.wrapped_storage
             post_save.disconnect(self.post_save_receiver, model_class)
-            post_delete.disconnect(self.post_delete_receiver, model_class) 
+            post_delete.disconnect(self.post_delete_receiver, model_class)
 
     # Low-level revision management methods.
-    
+
     def start(self):
         """
         Begins a revision for this thread.
-        
+
         This MUST be balanced by a call to `end`.  It is recommended that you
         leave these methods alone and instead use the revision context manager
         or the `create_on_success` decorator.
         """
         self._state.depth += 1
-        
+
     def is_active(self):
         """Returns whether there is an active revision for this thread."""
         return self._state.depth > 0
-    
+
     def assert_active(self):
         """Checks for an active revision, throwning an exception if none."""
         if not self.is_active():
             raise RevisionManagementError, "There is no active revision for this thread."
-    
+
     def add(self, obj):
         """Adds an object to the current revision."""
         self.assert_active()
         self._state.objects.add(obj)
-        
+
     def set_user(self, user):
         """Sets the user for the current revision"""
         self.assert_active()
         self._state.user = user
-        
+
     def get_user(self):
         """Gets the user for the current revision."""
         self.assert_active()
         return self._state.user
-    
+
     user = property(get_user,
                     set_user,
                     doc="The user for the current revision.")
-        
+
     def set_comment(self, comment):
         """Sets the comment for the current revision"""
         self.assert_active()
         self._state.comment = comment
-        
+
     def get_comment(self):
         """Gets the comment for the current revision."""
         self.assert_active()
         return self._state.comment
-    
+
     comment = property(get_comment,
                        set_comment,
                        doc="The comment for the current revision.")
-        
+
     def add_meta(self, cls, **kwargs):
         """Adds a class of meta information to the current revision."""
         self.assert_active()
         self._state.meta.append((cls, kwargs))
-        
+
     def invalidate(self):
         """Marks this revision as broken, so should not be commited."""
         self.assert_active()
         self._state.is_invalid = True
-        
+
     def is_invalid(self):
         """Checks whether this revision is invalid."""
         return self._state.is_invalid
-        
+
     def follow_relationships(self, object_set):
         """
         Follows all the registered relationships in the given set of models to
@@ -228,13 +229,20 @@ class RevisionManager(object):
                     if isinstance(related_field, models.ForeignKey):
                         if hasattr(obj, related_field.get_cache_name()):
                             delattr(obj, related_field.get_cache_name())
-                # Get the referenced obj(s).
                 try:
+                    field = obj._meta.get_field_by_name(relationship)[0]
+                except FieldDoesNotExist:
+                    raise TypeError
+                try:
+                    if hasattr(field, 'rel'):
+                        related_field_model = field.rel.to
+                    elif hasattr(field, 'model'):
+                        related_field_model = field.model
                     related = getattr(obj, relationship, None)
-                except obj._meta.get_field_by_name(relationship)[0].model.DoesNotExist:
+                except related_field_model.DoesNotExist:
                     continue
                 if isinstance(related, models.Model):
-                    _follow_relationships(related) 
+                    _follow_relationships(related)
                 elif isinstance(related, (models.Manager, QuerySet)):
                     for related_obj in related.all():
                         _follow_relationships(related_obj)
@@ -248,7 +256,7 @@ class RevisionManager(object):
                     _follow_relationships(parent_obj)
         map(_follow_relationships, object_set)
         return result_set
-        
+
     def end(self):
         """Ends a revision."""
         self.assert_active()
@@ -263,7 +271,7 @@ class RevisionManager(object):
                                                        comment=self._state.comment)
                     # Follow relationships.
                     revision_set = self.follow_relationships(self._state.objects)
-                    # Because we might have uncomitted data in models, we need to 
+                    # Because we might have uncomitted data in models, we need to
                     # replace the models in revision_set which might have come from the
                     # db, with the actual models sent to reversion.
                     diff = revision_set.difference(models)
@@ -291,9 +299,9 @@ class RevisionManager(object):
                         cls._default_manager.create(revision=revision, **kwargs)
             finally:
                 self._state.clear()
-        
+
     # Signal receivers.
-        
+
     def post_save_receiver(self, instance, sender, **kwargs):
         """Adds registered models to the current revision, if any."""
         if self.is_active():
@@ -311,18 +319,18 @@ class RevisionManager(object):
             pass
 
     # High-level revision management methods.
-        
+
     def __enter__(self):
         """Enters a block of revision management."""
         self.start()
-        
+
     def __exit__(self, exc_type, exc_value, traceback):
         """Leaves a block of revision management."""
         if exc_type is not None:
             self.invalidate()
         self.end()
         return False
-        
+
     def create_on_success(self, func):
         """Creates a revision when the given function exist successfully."""
         def _create_on_success(*args, **kwargs):
@@ -338,7 +346,7 @@ class RevisionManager(object):
             return result
         return wraps(func)(_create_on_success)
 
-        
+
 # A thread-safe shared revision manager.
 revision = RevisionManager()
 
